1a4528284576d817312b35b5d8a302db2a5b0b02
Aruna Venkataraman <aruna.venkatraman@gmail.com>
2011-10-08 15:14:04 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeatherMonster::Application.config.secret_token = 'e8e30e20c5bf8e27e4218a5c70ee4375fae770cb5d58ca2e5f64de8e35bbd3e7e540d7ea19e1581adef38c5e74aa682be453f5dd50eb315b3ae7ef2ed68fd119'
